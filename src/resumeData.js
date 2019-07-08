dcb081fbaed0b403f46f5d70008787b684f5bfd3
Aaron Le <ale2pwn@gmail.com>
2019-07-08 16:57:45 -0700

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Aaron Le",
+    "role": "Frontend Developer and Android Developer",
+    "linkedinId":"https://www.linkedin.com/in/aaron-le-864ab8162/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/leaagithub",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a recent graduate from California State University, Sacramento and pursuing a career in the technology industry. My favorite OS to use at the moment is Chrome OS and I love messing with crostini. Crostini has given me a lot of flexibility when working with project when I am allowed to have linux and chromeOS run side by side. Some of my favorite development tools are reactjs, reactjs-native, and android studio.",
+    "address":"Elk Grove, CA",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"California State University, Sacramento",
+        "specialization":"Bachelor of Science in Computer Science",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2018",
+        "Achievements":"Created many projects that allowed me to build a CS foundation. Projects were mostly done in java, but they were done to demonstrating my understanding of the fundamentals such as objects oriented programminng, data structure, and data algorithms. "
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"iHouseWeb",
+        "specialization":"Web Building",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2019",
+        "Achievements1":"Build many websites using the IDX platform that creates website in ReactJS" ,
+        "Achievements2":"Imported content to website components in HTML and CSS to complete the website",
+        "Achievements3":"Customized content using inline, internal, and external CSS style",
+        "Achievements4":"Handled all customization that customer requested through the ticket system"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"GISA (Geologist Information Site Assessment)",
+        "description":"Full Stack Android and Website Application",
+        "imgurl":"../images/portfolio/CalTransLogo.png"
+      },
+      {
+        "name":"SSCAP (Student Service Counselling Appointment Program)",
+        "description":"VueJs Website Application",
+        "imgurl":"../images/portfolio/SacStateLogo.jpg"
+      },
+      {
+        "name":"Spaceship Game",
+        "description":"Java Object Oriented Space Ship Game",  
+        "imgurl":"../images/portfolio/Spaceship.png"
+      },
+      {
+        "name":"Simple Sequence Repeat Characterizer",
+        "description":"Python Program",
+        "imgurl":"../images/portfolio/dna.png"
+      },
+      {
+        "name":"Rock, Paper, Scissors",
+        "description":"Android App Game",
+        "imgurl":"../images/portfolio/rockpaperscissorphoto.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
